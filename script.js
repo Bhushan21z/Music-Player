8419953f14c1eeefbdb2fb09610682afc1ba909a
Bhushan21z <82889656+Bhushan21z@users.noreply.github.com>
2022-01-26 00:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,96 +8,243 @@ let el4 = document.querySelector('.h4d')
 let open=0;
 cove=document.querySelector('.cove');
 
+let cpic11=document.querySelector('.cpic11');
+let cpic12=document.querySelector('.cpic12');
+let cpic13=document.querySelector('.cpic13');
+let cpic14=document.querySelector('.cpic14');
+let cpic15=document.querySelector('.cpic15');
+let sname11=document.querySelector('.sname11');
+let sname12=document.querySelector('.sname12');
+let sname13=document.querySelector('.sname13');
+let sname14=document.querySelector('.sname14');
+let sname15=document.querySelector('.sname15');
 
+
+let cpic21=document.querySelector('.cpic21');
+let cpic22=document.querySelector('.cpic22');
+let cpic23=document.querySelector('.cpic23');
+let cpic24=document.querySelector('.cpic24');
+let cpic25=document.querySelector('.cpic25');
+let sname21=document.querySelector('.sname21');
+let sname22=document.querySelector('.sname22');
+let sname23=document.querySelector('.sname23');
+let sname24=document.querySelector('.sname24');
+let sname25=document.querySelector('.sname25');
+
+//cpic11.style.backgroundImage= `url('${5}.jpg')`;
+
+
+let songname=[
+    "Udd Gaye","Brown Munde","Doja Cat","Drake","Rap-God","Excuses","Sterio","Dekh LE","Life goes on","Stay",
+]
+
+let songname2=[
+    "Udd Gaye","Brown Munde","Doja Cat","Drake","Rap-God","Excuses","Sterio","Dekh LE","Life goes on","Stay",
+]
+names=document.querySelector('.name');
+names.innerHTML= songname[0];
 song1=document.querySelector('.song11');
 song2=document.querySelector('.song12');
 song3=document.querySelector('.song13');
 song4=document.querySelector('.song14');
 song5=document.querySelector('.song15');
+
+
 song6=document.querySelector('.song21');
 song7=document.querySelector('.song22');
 song8=document.querySelector('.song23');
 song9=document.querySelector('.song24');
 song10=document.querySelector('.song25');
 
-let songIndex = 0;
+let songIndex1 = 1;
 let audioElement = new Audio('1.mp3');
-audioElement.src = `${songIndex+1}.mp3`;
+audioElement.src = `${songIndex1}.mp3`;
 let masterPlay = document.getElementById('masterPlay');
 
 let myProgressBar = document.getElementById('musicseekbar');
 let songItems = Array.from(document.getElementsByClassName('song'));
 let nexplay=document.querySelector('.mastplay');
 
+let nextpage=document.getElementById('nextpage');
+let prevpage=document.getElementById('prevpage');
+let t1=1,t2=2,t3=3,t4=4,t5=5;
+nextpage.addEventListener('click', ()=>{
+     songIndex1=songIndex1+1;
+    t1=t1+1;t2=t2+1;t3=t3+1;t4=t4+1;t5=t5+1;
+    if(t1==11) t1=1;
+    if(t2==11) t2=1;
+    if(t3==11) t3=1;
+    if(t4==11) t4=1;
+    if(t5==11) t5=1;
+    
+     cpic11.style.backgroundImage= `url('${t1}.jpg')`;
+     cpic12.style.backgroundImage= `url('${t2}.jpg')`;
+     cpic13.style.backgroundImage= `url('${t3}.jpg')`;
+     cpic14.style.backgroundImage= `url('${t4}.jpg')`;
+     cpic15.style.backgroundImage= `url('${t5}.jpg')`;
+     
+     sname11.innerHTML=songname[t1-1];
+     sname12.innerHTML=songname[t2-1];
+     sname13.innerHTML=songname[t3-1];
+     sname14.innerHTML=songname[t4-1];
+     sname15.innerHTML=songname[t5-1];
+})
+
+prevpage.addEventListener('click', ()=>{
+    songIndex1=songIndex1-1;
+   t1=t1-1;t2=t2-1;t3=t3-1;t4=t4-1;t5=t5-1;
+   if(t1==0) t1=10;
+   if(t2==0) t2=10;
+   if(t3==0) t3=10;
+   if(t4==0) t4=10;
+   if(t5==0) t5=10;
+   
+    cpic11.style.backgroundImage= `url('${t1}.jpg')`;
+    cpic12.style.backgroundImage= `url('${t2}.jpg')`;
+    cpic13.style.backgroundImage= `url('${t3}.jpg')`;
+    cpic14.style.backgroundImage= `url('${t4}.jpg')`;
+    cpic15.style.backgroundImage= `url('${t5}.jpg')`;
+    
+    sname11.innerHTML=songname[t1-1];
+    sname12.innerHTML=songname[t2-1];
+    sname13.innerHTML=songname[t3-1];
+    sname14.innerHTML=songname[t4-1];
+    sname15.innerHTML=songname[t5-1];
+})
+
+
+
+
 song1.addEventListener('click', ()=>{
-        songIndex=0;
-        audioElement.src = `${songIndex+1}.mp3`;
+        songIndex1=1;
+        audioElement.src = `${t1}.mp3`;
         audioElement.play();
         nexplay.style.backgroundImage = "url('pause.png')";
         cove.style.opacity=1;
+        names.innerHTML= songname[t1-1];
 })
 song2.addEventListener('click', ()=>{
-    songIndex=1;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex1=2;
+    audioElement.src = `${t2}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname[t2-1];
 })
 song3.addEventListener('click', ()=>{
-    songIndex=2;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex1=3;
+    audioElement.src = `${t3}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname[t3-1];
 })
 song4.addEventListener('click', ()=>{
-    songIndex=3;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex1=4;
+    audioElement.src = `${t4}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname[t4-1];
 })
 song5.addEventListener('click', ()=>{
-    songIndex=4;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex1=5;
+    audioElement.src = `${t5}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname[t5-1];
 })
+
+songIndex2=1;
+let nextpagec=document.getElementById('nextpagec');
+let prevpagec=document.getElementById('prevpagec');
+let f1=1,f2=2,f3=3,f4=4,f5=5;
+nextpagec.addEventListener('click', ()=>{
+     songIndex2=songIndex2+1;
+    f1=f1+1;f2=f2+1;f3=f3+1;f4=f4+1;f5=f5+1;
+    if(f1==11) f1=1;
+    if(f2==11) f2=1;
+    if(f3==11) f3=1;
+    if(f4==11) f4=1;
+    if(f5==11) f5=1;
+    
+     cpic21.style.backgroundImage= `url('${f1}.jpg')`;
+     cpic22.style.backgroundImage= `url('${f2}.jpg')`;
+     cpic23.style.backgroundImage= `url('${f3}.jpg')`;
+     cpic24.style.backgroundImage= `url('${f4}.jpg')`;
+     cpic25.style.backgroundImage= `url('${f5}.jpg')`;
+     
+     sname21.innerHTML=songname2[f1-1];
+     sname22.innerHTML=songname2[f2-1];
+     sname23.innerHTML=songname2[f3-1];
+     sname24.innerHTML=songname2[f4-1];
+     sname25.innerHTML=songname2[f5-1];
+})
+
+prevpagec.addEventListener('click', ()=>{
+    songIndex2=songIndex2-1;
+   f1=f1-1;f2=f2-1;f3=f3-1;f4=f4-1;f5=f5-1;
+   if(f1==0) f1=10;
+   if(f2==0) f2=10;
+   if(f3==0) f3=10;
+   if(f4==0) f4=10;
+   if(f5==0) f5=10;
+   
+    cpic21.style.backgroundImage= `url('${f1}.jpg')`;
+    cpic22.style.backgroundImage= `url('${f2}.jpg')`;
+    cpic23.style.backgroundImage= `url('${f3}.jpg')`;
+    cpic24.style.backgroundImage= `url('${f4}.jpg')`;
+    cpic25.style.backgroundImage= `url('${f5}.jpg')`;
+    
+    sname21.innerHTML=songname2[f1-1];
+    sname22.innerHTML=songname2[f2-1];
+    sname23.innerHTML=songname2[f3-1];
+    sname24.innerHTML=songname2[f4-1];
+    sname25.innerHTML=songname2[f5-1];
+})
+
+
+
 song6.addEventListener('click', ()=>{
-    songIndex=5;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex2=1;2
+    audioElement.src = `${f1}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname2[f1-1];
 })
 song7.addEventListener('click', ()=>{
-    songIndex=6;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex2=2;
+    audioElement.src = `${f2}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname2[f2-1];
 })
 song8.addEventListener('click', ()=>{
-    songIndex=7;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex2=3;
+    audioElement.src = `${f3}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname2[f3-1];
 })
 song9.addEventListener('click', ()=>{
-    songIndex=8;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex2=4;
+    audioElement.src = `${f4}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname2[f4-1];
 })
 song10.addEventListener('click', ()=>{
-    songIndex=9;
-    audioElement.src = `${songIndex+1}.mp3`;
+    songIndex2=5;
+    audioElement.src = `${f5}.mp3`;
     audioElement.play();
     nexplay.style.backgroundImage = "url('pause.png')";
     cove.style.opacity=1;
+    names.innerHTML= songname2[f5-1];
 })
 
 masterPlay.addEventListener('click', ()=>{
@@ -123,43 +270,58 @@ myProgressBar.addEventListener('change', ()=>{
 
 
 
-
+/*
 document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
+    if(songIndex1>=10){
+        songIndex1 = 1
     }
     else{
-        songIndex += 1;
+        songIndex1 += 1;
     }
-    audioElement.src = `${songIndex+1}.mp3`;
+    audioElement.src = `${songIndex1}.mp3`;
     audioElement.currentTime = 0;
     audioElement.play();
     cove.style.opacity=1;
 })
+
 
 document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+    songIndex1-=1;
+    if(songIndex1==0){
+        songIndex1=10;
+    }
+    audioElement.src = `${songIndex1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    cove.style.opacity=1;
+})*/
+
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex2>=10){
+        songIndex2 = 1
     }
     else{
-        songIndex -= 1;
+        songIndex2 += 1;
     }
-    audioElement.src = `${songIndex+1}.mp3`;
+    audioElement.src = `${songIndex2}.mp3`;
     audioElement.currentTime = 0;
     audioElement.play();
     cove.style.opacity=1;
 })
 
 
+document.getElementById('previous').addEventListener('click', ()=>{
+    songIndex2-=1;
+    if(songIndex2==0){
+        songIndex2=10;
+    }
+    audioElement.src = `${songIndex2}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    cove.style.opacity=1;
+})
 
-let songname=[
-    "Udd Gaye","Brown Munde","Doja Cat","Drake","Rap-God","Excuses","Sterio","Dekh LE","Lige goes on","Stay",
-]
-names=document.querySelector('.name');
-names.innerHTML= songname[songIndex];
-setInterval(()=>{
-    names.innerHTML= songname[songIndex];
-},200)
 
 
 
@@ -185,7 +347,6 @@ buttonfb.addEventListener('click', ()=>{
         el2.innerHTML="";
         el3.innerHTML="";
         el4.innerHTML="";
-        
        open=0;
     }
 })
